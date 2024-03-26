227f388c4611e37d67f0ae6103b949cef458729e
Janmejay Buranpuri <jburanpu@uwo.ca>
2024-03-26 13:54:47 -0400

    fixed sample model

--- backend/matchpredictor/predictors/sample_model.py
+++ backend/matchpredictor/predictors/sample_model.py
@@ -4,8 +4,7 @@ from matchpredictor.predictors.predictor import Prediction, Predictor
 
 class SampleModel(Predictor):
     def predict(self, fixture: Fixture) -> Prediction:
-        if Outcome.HOME > Outcome.AWAY:
+        if fixture.home_team.name < fixture.away_team.name:
             return Prediction(outcome=Outcome.HOME)
-
         else:
             return Prediction(outcome=Outcome.AWAY)
